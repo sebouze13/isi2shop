c0360f04a27508dbe137a0b72feeecd455457b94
Unknown <percia.sebastien@gmail.com>
2018-09-10 10:44:42 -0400

    Creation templating
    Header + footer

---
+++ views/footer.php
@@ -0,0 +1,143 @@
+<!-- FOOTER START -->
+<div class="footer">
+    <div class="container">
+        <div class="footer-inner">
+            <div class="footer-middle">
+                <div class="row">
+                    <div class="col-xl-3 f-col">
+                        <div class="footer-static-block"> <span class="opener plus"></span>
+                            <h3 class="title">address<span></span></h3>
+                            <ul class="footer-block-contant address-footer">
+                                <li class="item"> <i class="fa fa-map-marker"> </i>
+                                    <p><em style="font-weight: bold;">Inst. Supérieur Informatique</em><br/>
+                                        255 Crémazie Est, #100<br/>
+                                        Montréal, Québec, H2M 1M2</p>
+                                </li>
+                                <li class="item"> <i class="fa fa-envelope"> </i>
+                                    <p> <a href="#">infoservices@isi2buy.com </a> </p>
+                                </li>
+                                <li class="item"> <i class="fa fa-phone"> </i>
+                                    <p>(+1) 514-842-2426</p>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-xl-3 f-col">
+                        <div class="footer-static-block"> <span class="opener plus"></span>
+                            <h3 class="title">Help <span></span></h3>
+                            <ul class="footer-block-contant link">
+                                <li><a href="#">Gift Cards</a></li>
+                                <li><a href="#">Order Status</a></li>
+                                <li><a href="#">Free Shipping</a></li>
+                                <li><a href="#">Return & Exchange </a></li>
+                                <li><a href="#">International</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-xl-3 f-col">
+                        <div class="footer-static-block"> <span class="opener plus"></span>
+                            <h3 class="title">Guidance <span></span></h3>
+                            <ul class="footer-block-contant link">
+                                <li><a href="#"> Delivery information</a></li>
+                                <li><a href="#"> Privacy Policy</a></li>
+                                <li><a href="#"> Terms & Conditions</a></li>
+                                <li><a href="#"> Contact</a></li>
+                                <li><a href="#"> Sitemap</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-xl-3 f-col">
+                        <div class="footer-static-block"> <span class="opener plus"></span>
+                            <h3 class="title">Information <span></span></h3>
+                            <ul class="footer-block-contant link">
+                                <li><a href="#">Gift Cards</a></li>
+                                <li><a href="#">Order Status</a></li>
+                                <li><a href="#">Free Shipping</a></li>
+                                <li><a href="#">Return & Exchange </a></li>
+                                <li><a href="#">International</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <hr>
+            <div class="footer-bottom ">
+                <div class="row mtb-30">
+                    <div class="col-lg-6 ">
+                        <div class="copy-right ">© 2018  All Rights Reserved. Design By <a href="#">Aaryaweb</a></div>
+                    </div>
+                    <div class="col-lg-6 ">
+                        <div class="footer_social pt-xs-15 center-sm">
+                            <ul class="social-icon">
+                                <li><div class="title">Follow us on :</div></li>
+                                <li><a title="Facebook" class="facebook"><i class="fa fa-facebook"> </i></a></li>
+                                <li><a title="Twitter" class="twitter"><i class="fa fa-twitter"> </i></a></li>
+                                <li><a title="Linkedin" class="linkedin"><i class="fa fa-linkedin"> </i></a></li>
+                                <li><a title="RSS" class="rss"><i class="fa fa-rss"> </i></a></li>
+                                <li><a title="Pinterest" class="pinterest"><i class="fa fa-pinterest"> </i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <hr>
+                <div class="row align-center mtb-30 ">
+                    <div class="col-12 ">
+                        <div class="site-link">
+                            <ul>
+                                <li><a href="#">About Us</a></li>
+                                <li><a href="#">Contact Us</a></li>
+                                <li><a href="#">Customer </a></li>
+                                <li><a href="#">Service</a></li>
+                                <li><a href="#">Privacy</a></li>
+                                <li><a href="#">Policy </a></li>
+                                <li><a href="#">Accessibility</a></li>
+                                <li><a href="#">Directory </a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="row align-center">
+                    <div class="col-12 ">
+                        <div class="payment">
+                            <ul class="payment_icon">
+                                <li class="visa"><a href="#"><img src="images/pay1.png" alt="Stylexpo"></a></li>
+                                <li class="discover"><a href="#"><img src="images/pay2.png" alt="Stylexpo"></a></li>
+                                <li class="paypal"><a href="#"><img src="images/pay3.png" alt="Stylexpo"></a></li>
+                                <li class="vindicia"><a href="#"><img src="images/pay4.png" alt="Stylexpo"></a></li>
+                                <li class="atos"><a href="#"><img src="images/pay5.png" alt="Stylexpo"></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="scroll-top">
+    <div class="scrollup"></div>
+</div>
+<!-- FOOTER END -->
+</div>
+<script src="js/jquery-1.12.3.min.js"></script>
+<script src="../../../../cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/jquery.downCount.js"></script>
+<script src="js/jquery-ui.min.js"></script>
+<script src="js/fotorama.js"></script>
+<script src="js/jquery.magnific-popup.js"></script>
+<script src="js/owl.carousel.min.js"></script>
+<script src="js/custom.js"></script>
+
+<script>
+    /* ------------ Newslater-popup JS Start ------------- */
+    $(window).load(function() {
+        // $.magnificPopup.open({
+        //   items: {src: '#newslater-popup'},type: 'inline'}, 0);
+    });
+    /* ------------ Newslater-popup JS End ------------- */
+</script>
+
+</body>
+
+<!-- Mirrored from aaryaweb.info/html/stylexpo/stx002/ by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 04 Sep 2018 12:58:15 GMT -->
+</html>

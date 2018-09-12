299ba95a3c8d8e113f0989baa2b47f478f69d01a
Blain <blain.gui@gmail.com>
2018-09-12 14:28:48 -0400

    mise a jour bd

--- bdd/isi2shop.sql
+++ bdd/isi2shop.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  lun. 10 sep. 2018 à 15:48
+-- Généré le :  mer. 12 sep. 2018 à 18:26
 -- Version du serveur :  5.7.22-log
 -- Version de PHP :  5.6.35
 
@@ -30,9 +30,19 @@ SET time_zone = "+00:00";
 
 DROP TABLE IF EXISTS `categorie`;
 CREATE TABLE IF NOT EXISTS `categorie` (
-  `id` int(1) NOT NULL,
-  `libelle` varchar(30) NOT NULL
+  `id` int(11) NOT NULL,
+  `libelle` varchar(30) NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `categorie`
+--
+
+INSERT INTO `categorie` (`id`, `libelle`) VALUES
+(1, 'homme'),
+(2, 'femme'),
+(3, 'enfant');
 
 -- --------------------------------------------------------
 
@@ -75,7 +85,24 @@ CREATE TABLE IF NOT EXISTS `produit` (
   `qte_dispo` int(2) NOT NULL,
   `id` int(11) NOT NULL AUTO_INCREMENT,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `produit`
+--
+
+INSERT INTO `produit` (`id_cat`, `libelle`, `description`, `img`, `prix`, `qte_dispo`, `id`) VALUES
+(1, 'Manteau', 'Manteau marron', '1.jpg', '80.00', 50, 1),
+(1, 'Montre', 'Montre en or', '2.jpg', '300.00', 50, 2),
+(1, 'Appareil photo', 'Appareil photo super-puissant', '3.jpg', '100.00', 50, 3),
+(1, 'Enceinte', 'Enceinte Samsung sophistiquée', '4.jpg', '100.00', 50, 4),
+(2, 'Echarpe', 'Echarpe burberry', '1.jpg', '50.00', 50, 5),
+(2, 'Bracelet', 'Bracelet en argent incassable', '2.jpg', '80.00', 50, 6),
+(2, 'Sac', 'Sac à main Kim Kardashian', '3.jpg', '200.00', 50, 7),
+(2, 'Lunettes', 'Lunettes de soleil du futur', '4.jpg', '20.00', 50, 8),
+(2, 'Talon', 'Talons-sandale pour chaussette', '5.jpg', '70.00', 50, 9),
+(2, 'Manteau', 'Manteau de fourure', '6.jpg', '150.00', 50, 10),
+(2, 'Bague', 'Bague en diamant', '7.jpg', '500.00', 50, 11);
 
 -- --------------------------------------------------------
 
